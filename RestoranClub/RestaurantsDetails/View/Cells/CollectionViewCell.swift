9820d5eff5a2680058710ccf2a86c0d144fa137d
Nikolay <k70456523@gmail.com>
2021-05-19 21:28:07 +0700

    Added: - Broken collectionview

---
+++ RestoranClub/RestaurantsDetails/View/Cells/CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewCell.swift
+//  RestoranClub
+//
+//  Created by Николай on 19.05.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var authorName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+}
--- RestoranClub/RestaurantsDetails/View/Cells/TableViewCellReview.swift
+++ RestoranClub/RestaurantsDetails/View/Cells/TableViewCellReview.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class TableViewCellReview: UITableViewCell {
 
-    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.register(UINib.init(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "collectionViewID")
     }
     func configure(model: Restorant) {
         
-        userNameLabel.text = "3"
     }
 
 
@@ -26,6 +29,21 @@ class TableViewCellReview: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+}
+
+extension TableViewCellReview: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewID", for: indexPath as IndexPath) as! CollectionViewCell
+        
+        return cell
     }
     
 }
